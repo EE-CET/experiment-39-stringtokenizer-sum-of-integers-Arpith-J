426ba921c35193e2ae1a87200a87c0adfb26f0ef
Arpith Joseph <arpithjoseph2005@gmail.com>
2026-03-04 22:05:15 +0530

    Code added

--- src/TokenSum.java
+++ src/TokenSum.java
@@ -4,10 +4,17 @@ import java.util.StringTokenizer;
 public class TokenSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // TODO: Read the entire line of integers
-        // TODO: Create a StringTokenizer object for the line
-        // TODO: Iterate through tokens (hasMoreTokens)
-        // TODO: Parse each token to int and add to sum
-        // TODO: Print the sum
+        // Read the entire line of integers
+        // Create a StringTokenizer object for the line
+        // Iterate through tokens (hasMoreTokens)
+        // Parse each token to int and add to sum
+        // Print the sum
+        String s = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(s," ");
+        int sum = 0;
+        while(st.hasMoreTokens()){
+            sum += Integer.parseInt(st.nextToken());
+        }
+        System.out.println(sum);
     }
 }
